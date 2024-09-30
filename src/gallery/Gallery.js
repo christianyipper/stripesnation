f88f03930b78572d62f3e520e683af7443da11b9
Christian <christianyippers@gmail.com>
2024-09-30 16:27:07 -0700

    Gallery Grid Layout

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -1,7 +1,28 @@
 const Gallery = () => {
     return (
-        <div className='App'>
+        <div className='gallery'>
             <h1>Coming Soon</h1>
+            <section className="photo-grid grid-top">
+                <div className="a1">A1</div>
+                <div className="a2">A2</div>
+                <div className="a3">A3</div>
+                <div className="a4">A4</div>
+                <div className="a5">A5</div>
+            </section>
+            <section className="photo-grid grid-mid">
+                <div className="b1">B1</div>
+                <div className="b2">B2</div>
+                <div className="b3">B3</div>
+                <div className="b4">B4</div>
+                <div className="b5">B5</div>
+            </section>
+            <section className="photo-grid grid-bot">
+                <div className="c1">C1</div>
+                <div className="c2">C2</div>
+                <div className="c3">C3</div>
+                <div className="c4">C4</div>
+                <div className="c5">C5</div>
+            </section>
         </div>
     )
 }
